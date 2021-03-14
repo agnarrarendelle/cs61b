3e867d9af9a6920e9662c25f134f564ea2d3e073
Matt <danny20001102@gmail.com>
2021-03-15 00:50:32 +0800

    IntList recursive get() method done

--- class-walkthrough/week1/IntList.java
+++ class-walkthrough/week1/IntList.java
@@ -24,11 +24,19 @@ public class IntList {
         }
         return totalSize;
     }
+    /** Return the ith item from the IntList */
+    public int get(int i){
+        if(i == 0){
+            return this.first;
+        }else{
+            return this.rest.get(--i);
+        }
+    }
 
     public static void main(String[] args) {
         IntList L = new IntList(15,null);
         L = new IntList(10, L);//create a new IntList where the rest is the old IntList
         L = new IntList(5, L);
-        System.out.print(L.size());
+        System.out.print(L.get(1));
     }
 }
