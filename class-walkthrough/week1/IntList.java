f2bf538f36e139110a9565f73b547fb0c2858f4d
Matt <danny20001102@gmail.com>
2021-03-14 23:59:44 +0800

    IntList recursive size() method done

--- class-walkthrough/week1/IntList.java
+++ class-walkthrough/week1/IntList.java
@@ -7,9 +7,18 @@ public class IntList {
         rest = r;
     }
 
+    public int size(){
+        if(rest == null){
+            return 1;
+        }else{
+            return 1 + this.rest.size();
+        }
+    }
+
     public static void main(String[] args) {
         IntList L = new IntList(15,null);
         L = new IntList(10, L);//create a new IntList where the rest is the old IntList
         L = new IntList(5, L);
+        System.out.print(L.size());
     }
 }
