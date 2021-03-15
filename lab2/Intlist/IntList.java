00e389b45a5e51afd05f40d2c5a582a44bd338b2
Matt <danny20001102@gmail.com>
2021-03-15 23:42:51 +0800

    lab2 test 2 done

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -94,8 +94,23 @@ public class IntList {
      * * elements of B.  May NOT modify items of A.  Use 'new'.
      */
     public static IntList catenate(IntList A, IntList B) {
-        //TODO:  fill in method
-        return null;
+        if (A == null){
+            return B;
+        }
+        IntList remain = new IntList(A.first, null);
+        IntList ptr = remain;
+        A = A.rest;
+        while(A != null){
+            ptr.rest = new IntList(A.first, null);
+            A = A.rest;
+            ptr = ptr.rest;
+        }
+        while(B != null){
+            ptr.rest = new IntList(B.first, null);
+            B = B.rest;
+            ptr = ptr.rest;
+        }
+        return remain;
     }
 
 
