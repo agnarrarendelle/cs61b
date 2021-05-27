f4304c77b4feec49c1da76dd2ab16832a9c944a1
Matt <danny20001102@gmail.com>
2021-05-28 00:42:24 +0800

    lab9 using Hashtable

--- lab9-2019/MyTrieSet.java
+++ lab9-2019/MyTrieSet.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Hashtable;
 import java.util.List;
 
 public class MyTrieSet<V> implements TrieSet61B{
@@ -5,17 +7,20 @@ public class MyTrieSet<V> implements TrieSet61B{
     private Node root;
 
     private static class Node{
-        private boolean isKey;
-        private Node[] next ;
-
-        public Node(){
-            isKey = false;
-            next = new Node[R];
-        }
+       private char character;
+       private boolean isKey;
+       private Hashtable<Character, Node> next;
+       private Node(char c, boolean isKey){
+           this.character = c;
+           this.isKey = isKey;
+           this.next = new Hashtable<>();
+       }
     }
 
-    public MyTrieSet(){
 
+    //Initialize the root witt a random character
+    public MyTrieSet(){
+        root = new Node('@', false);
     }
 
     private void isKeyValid(String key){
@@ -28,7 +33,7 @@ public class MyTrieSet<V> implements TrieSet61B{
     /** Clears all items out of Trie */
     @Override
     public void clear(){
-        root = null;
+        root.next.clear();
     }
 
 
@@ -37,22 +42,22 @@ public class MyTrieSet<V> implements TrieSet61B{
     /** Returns true if the Trie contains KEY, false otherwise */
     @Override
     public boolean contains(String key){
-       return get(root, key, 0) != null;
+        isKeyValid(key);
+
+        Node current = root;
+        for(int i = 0; i < key.length(); i++){
+            char ch = key.charAt(i);
+            if(!current.next.containsKey(ch)){
+                return false;
+            }
+            current = current.next.get(ch);
+        }
+        return true;
     }
 
 
 
-    private Node get(Node node, String key, int count){
-        if(node == null){
-            return null;
-        }
-        if(count == key.length()){
-            return node;
-        }
 
-        char c = key.charAt(count);
-        return get(node.next[c], key, count+1);
-    }
 
 
     /** Inserts string KEY into Trie */
@@ -60,33 +65,96 @@ public class MyTrieSet<V> implements TrieSet61B{
     public void add(String key){
         isKeyValid(key);
 
-        root = add(root, key, 0);
+        if(contains(key)){
+            return;
+        }
+        add(root, key);
     }
 
-    private Node add(Node node, String key, int count){
-        if(node == null){
-            node = new Node();
-        }
-        if(count == key.length()){
-            node.isKey = true;
-            return node;
+    private void add(Node node, String key){
+        //put the first character of the key into the tire
+        char ch = key.charAt(0);
+        if(!node.next.containsKey(ch)){
+            node.next.put(ch, new Node(ch, false));
         }
 
-        char c = key.charAt(count);
-        node.next[c] = add(node.next[c], key, count+1);
-        return node;
+        //recursively do the same to the rest of the string
+        Node nextNode = node.next.get(ch);
+        if(key.length() == 1){
+            nextNode.isKey = true;
+        }else{
+            add(nextNode, key.substring(1));
+        }
     }
+
+
 
 
     /** Returns a list of all words that start with PREFIX */
-    @Override
-    public List<String> keysWithPrefix(String prefix){
-        throw new UnsupportedOperationException();
+    /**
+     * Returns all keys in the trie set.
+     *
+     * @param prefix the prefix string
+     * @return the list of keys with the given prefix
+     * @throws IllegalArgumentException if {@code key} is {@code null} or size is 0
+     */
+
+    public List<String> keysWithPrefix(String prefix) {
+        if (prefix == null || prefix.length() < 1) {
+            throw new IllegalArgumentException("argument to keysWithPrefix() is null or size is 0");
+        }
+        Node node = getEndNode(prefix);
+        if (node == null) {
+            return null;
+        }
+        ArrayList<String> keys = new ArrayList<>();
+        for (Character ch : node.next.keySet()) {
+            collect(prefix + ch, keys,node.next.get(ch));
+        }
+        return keys;
     }
 
-    private void collect(List collected, String key, Node node){
-
+    /**
+     * Helper function to get the last node of the given {@code key}.
+     *
+     * @param key the key
+     * @return the last node of the given {@code key}.
+     */
+    private Node getEndNode(String key) {
+        Node curr = root;
+        for (int i = 0; i < key.length(); i++) {
+            char ch = key.charAt(i);
+            if (!curr.next.containsKey(ch)) {
+                return null;
+            }
+            curr = curr.next.get(ch);
+        }
+        return curr;
     }
+
+    public List<String> collect(){
+        if(root == null){
+            return null;
+        }
+        List<String> allKeys = new ArrayList<>();
+
+        for(char ch: root.next.keySet()){
+            collect(String.valueOf(ch), allKeys, root.next.get(ch));
+        }
+        return allKeys;
+    }
+
+    private void collect(String k, List<String> collectedString, Node node){
+        if(node.isKey){
+            collectedString.add(k);
+        }
+
+        for(Character ch: node.next.keySet()){
+            collect(k+ch, collectedString, node.next.get(ch));
+        }
+    }
+
+
 
 
     /** Returns the longest prefix of KEY that exists in the Trie
@@ -98,6 +166,5 @@ public class MyTrieSet<V> implements TrieSet61B{
         throw new UnsupportedOperationException();
 
 
-        fdssdfd
     }
 }
